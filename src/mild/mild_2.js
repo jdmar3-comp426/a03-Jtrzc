ac88c8d486eb8f9d967ec5318c5f0fc3aa8a0894
Jtrzc <Jtrzc@ad.unc.edu>
2021-10-28 17:05:27 -0400

    return holder added to ND

--- src/mild/mild_2.js
+++ src/mild/mild_2.js
@@ -82,6 +82,7 @@ export function removeKey(object, key) {
 export function removeKeyNonDestructive(object, key) {
       const holder = {...object};
       delete holder[key];
+      return holder;
 }
 
 /**

e847e73f33ca70572e62f6455defa598ab9f78e9
Jtrzc <Jtrzc@ad.unc.edu>
2021-10-28 16:36:46 -0400

    NonDestructive Changes

--- src/mild/mild_2.js
+++ src/mild/mild_2.js
@@ -79,7 +79,7 @@ export function removeKey(object, key) {
  If only `removeKeyNonDestructive` was called, nothing would have changed.
  */
 export function removeKeyNonDestructive(object, key) {
-      let holder = [...object];
+      const holder = {...object};
       delete holder.key;
       return holder;
 }
